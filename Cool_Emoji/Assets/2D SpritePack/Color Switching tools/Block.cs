f27f2a086972cdca9c0d651eb90cfde5cf826a55
Davin Seju <davsej100@gmail.com>
2019-06-12 18:54:55 -0700

    Added PreFabs
    binary switches work finally!!!
    Multicolor switches are in progress

---
+++ Cool_Emoji/Assets/2D SpritePack/Color Switching tools/Block.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    Color c;
+    Renderer rend;
+    BoxCollider2D cd;
+    Rigidbody2D rd;
+
+   public Color getColor() {
+        return rend.material.GetColor("_Color");
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend=GetComponent<Renderer>();
+        //  rend.material.SetColor("_Color",Color.red); 
+
+        cd = gameObject.GetComponent<BoxCollider2D>() as BoxCollider2D;
+        rd = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+        rd.bodyType = RigidbodyType2D.Static;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rend.material.GetColor("_Color").Equals(Color.blue))
+        {
+            cd.enabled = true;
+
+        }
+        else
+        {
+            cd.enabled = false;
+        }
+
+    }
+
+    public void ChangeColor(Color c) {
+
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        rend.material.SetColor("_Color", c);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        
+
+
+    }
+
+
+
+    }
+
+
---
+++ Cool_Emoji/Assets/2D SpritePack/Color Switching tools/Button.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    public Sprite activated;
+    public Sprite nonactivated;
+
+    // Start is called before the first frame update
+    SpriteRenderer spriteRenderer;
+    List<GameObject> blocks;
+    List<Block> block_script;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = activated;
+        blocks = new List<GameObject>();
+        block_script = new List<Block>();
+        CircleCollider2D cd = gameObject.AddComponent<CircleCollider2D>() as CircleCollider2D;
+        Rigidbody2D rd = gameObject.AddComponent<Rigidbody2D>() as Rigidbody2D;
+        rd.bodyType = RigidbodyType2D.Static;
+        cd.radius = 1.426f;
+        foreach (Transform child in transform)
+        {
+            blocks.Add(child.gameObject);
+
+
+
+        }
+
+        foreach (GameObject blo in blocks)
+        {
+          
+            block_script.Add(blo.GetComponent<Block>());
+
+        }
+        foreach (Block b in block_script)
+        {
+            b.ChangeColor(Color.red);
+
+        }
+        // Vector2 off = Vector2(-0.626f, 0.8475316f);
+        // cd.offset = off;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Debug.Log("changed");
+            foreach (Block b in block_script)
+            {
+                b.ChangeColor(Color.red);
+
+            }
+
+        }
+        
+    }
+
+
+        void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Bullet"){
+            change();
+
+            Destroy(col.gameObject);
+            foreach (Block b in block_script)
+            {
+               b.ChangeColor(Color.blue);
+
+            }
+
+        }
+
+    }
+
+    void change()
+    {
+
+        if (spriteRenderer.sprite.Equals(activated)) {
+            spriteRenderer.sprite = nonactivated;
+
+        }
+        else {
+            spriteRenderer.sprite = activated;
+        }
+
+    }
+    
+    
+
+}
